ee35ab28360dcb670319468a4318ecd9ba5077f7
Raffael Daltoé <69999094+raffael-daltoe@users.noreply.github.com>
2023-12-05 19:06:36 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,30 @@
+sudo apt install  \
+cpio \
+libgtk-3-dev \
+inetutils-tools \
+libxcrypt-source \
+libncurses5 \
+libncurses5-dev \
+libpng-dev \
+libpng++-dev \
+libimage-png-libpng-perl \
+libpng-tools \
+libpng16-16 \
+libpnglite-dev \
+xorgxrdp \
+xorg-server-source-hwe-18.04 \
+xorg-server-source \
+xorg-docs-core \
+xorg-docs \
+xorg-dev \
+xorg \
+#digilent.adept.runtime \
+#digilent.adept.utilities \
+fxload \
+#vivado-boards-git
+
+sudo /tools/Xilinx/Vivado/2023.2/data/xicom/cable_drivers/lin64/install_script/install_drivers/install_drivers
+
+sudo /tools/Xilinx/Vivado/2023.2/data/xicom/cable_drivers/lin64/install_script/install_drivers/setup_pcusb
+
+cp 52-xilinx-*.* /etc/udev/rules.d
